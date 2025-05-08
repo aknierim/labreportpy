467036613528c2aca1410553c59a30c030f199ac
Anno Knierim <anno.knierim@tu-dortmund.de>
2025-05-08 20:16:01 +0200

    Add UXD parser to init

--- labreportpy/io/__init__.py
+++ labreportpy/io/__init__.py
@@ -1,7 +1,9 @@
 from .print_df import print_df
 from .table_writer import TableWriter
+from .uxd_parser import UXDparser
 
 __all__ = [
     "print_df",
-    "TableWriter"
+    "TableWriter",
+    "UXDparser",
 ]
